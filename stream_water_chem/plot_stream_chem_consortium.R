7a7c8535cbd689f15a002044990d3041f571acb8
AnneMariePanetta <anne.panetta@gmail.com>
2026-07-16 13:23:37 -0600

    Revised code for the public, and updated figure axes and labels.

--- stream_water_chem/plot_stream_chem_consortium.R
+++ stream_water_chem/plot_stream_chem_consortium.R
@@ -1,150 +1,167 @@
-# Code to plot concentrations
-# Code developed by Sarah Elmendorf (2023)
-# Code updated by Anne Marie Panetta (2026)
-# see nwt-8 long-term trends for flow-normalized workflow if desired
+# ============================================================================
+# Niwot Ridge LTER Data Dashboard: Stream Chemistry Anomaly Figures
+# ============================================================================
+#
+# Purpose:
+#   Reads stream water chemistry data from six Niwot Ridge sites and
+#   produces:
+#
+#     1. sulfate_anom_sept.png     September sulfate anomaly, by site
+#     2. nitrate_anom_summer.png   Summer (May-Sep) nitrate anomaly, by site
+#
+# Input:
+#   data/albisolu.nc.data.csv   -- Albion
+#   data/gre4solu.nc.data.csv   -- Green Lake 4
+#   data/saddsolu.nc.data.csv   -- Saddle Stream
+#   data/martsolu.nc.data.csv   -- Martinelli basin
+#   data/grrgsolu.nc.data.csv   -- Green Lake 5 Rock Glacier
+#   data/gre5solu.nc.data.csv   -- Green Lake 5 Outlet
+#
+# Output:
+#   Two PNG figures written to figures_dir (see file names above).
+#
+# Notes:
+#   See the nwt-8 long-term trends repo for a flow-normalized workflow, if
+#   that's more appropriate for your question than the raw concentrations
+#   used here.
+#
+# Original analysis: Sarah C. Elmendorf, 2023
+# Revised and updated for the Data Dashboard: Anne Marie Panetta, 2026
+# ============================================================================
 
-# setup#########
-# clean up enviro, read in needed libraries
+# -- SETUP --------------------------------------------------------------
+
 rm(list = ls())
-setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 
-# library(readxl)
+
 library(tidyverse)
-# library(vegan)
+library(ggthemes)  # theme_hc()
+library(ggpmisc)   # symmetric_limits() for scale_y_continuous
 options(stringsAsFactors = F)
+
 theme_set(theme_bw())
 
-####New from Anne Marie to download data from EDI####
-# only need to download once
-download_data <- TRUE
+setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 
 data_dir <- "data"
 figures_dir <- "figures"
 
-# Create figures directory if it doesn't exist
 if (!dir.exists(figures_dir)) {
   dir.create(figures_dir, recursive = TRUE)
 }
 
-# download data -----------------------------------------------------------
-# Note: if you have already downloaded SOME data, the read_data_package_archive
-# function will not work as it does not want to overwrite. Clear your /data
-# directories and then return to run the following code.
+# Set to TRUE to (re)download source data from EDI. Only needs to be run
+# once, or when a newer package version is released.
+download_data <- FALSE
+
+# -- DOWNLOAD DATA FROM EDI ---------------------------------------------
+# Note: if you have already downloaded SOME data, read_data_package_archive()
+# will not overwrite existing files. Clear the /data directory first if you
+# need to force a fresh download.
+
 if (download_data) {
-  # download the data from EDI
-  # discharge inputs used are:
-  # knb-lter-nwt.102 (albion)
-  # knb-lter-nwt.105 (gl4)
-  # knb-lter-nwt.111 (martinelli)
-  # knb-lter-nwt.74 (saddle)
-  
-  # no discharge but if you just want trends
-  # there are some others in just select yrs, etc.
-  # 163 is gr5rock glacier; 162 is watershed flume; 213 is soddie, et
-  # 109 is gl5#rg
-  
   if (!dir.exists(data_dir)) {
     dir.create(data_dir, recursive = TRUE)
   }
   
-  scope <- "knb-lter-nwt" # Niwot scope
+  scope <- "knb-lter-nwt"  # Niwot scope
+  
+  # Discharge/chemistry inputs used, with current citations (update as
+  # package versions change):
+  #
+  #   Albion: 103, 1982-ongoing
+  #     Caine, T., J. Morse, S. Yevak, H. Brandes, and Niwot Ridge LTER.
+  #     2025. Stream water chemistry data for Albion site, 1982 - ongoing.
+  #     ver 15. Environmental Data Initiative.
+  #     https://doi.org/10.6073/pasta/39b37d16657733f1d661de4c727568a3
+  #
+  #   Green Lake 4: 108, 1982-ongoing
+  #     Caine, T., J. Morse, S. Yevak, H. Brandes, and Niwot Ridge LTER.
+  #     2025. Stream water chemistry data for Green Lake 4, 1982 - ongoing.
+  #     ver 14. Environmental Data Initiative.
+  #     https://doi.org/10.6073/pasta/cdbd25e2ce7a8b17f785bd4f07aba4fe
+  #
+  #   Martinelli basin: 112, 1984-ongoing
+  #     Caine, T., J. Morse, S. Yevak, H. Brandes, and Niwot Ridge LTER.
+  #     2025. Stream water chemistry data for Martinelli basin,
+  #     1984 - ongoing. ver 5. Environmental Data Initiative.
+  #     https://doi.org/10.6073/pasta/4a761ed1f7b24d25c3eb870662213789
+  #
+  #   Green Lake 5 Rock Glacier: 163, 1998-ongoing
+  #     Caine, T., J. Morse, S. Yevak, H. Brandes, and Niwot Ridge LTER.
+  #     2025. Stream water chemistry data for Green Lake 5 Rock Glacier,
+  #     1998 - ongoing. ver 4. Environmental Data Initiative.
+  #     https://doi.org/10.6073/pasta/990a451879638d17eda4526215114390
+  #
+  #   Green Lake 5 Outlet: 109, 1984-ongoing (through ~2021)
+  #     Caine, T. 2021. Stream water chemistry data for Green Lake 5
+  #     outlet, 1984 - ongoing. ver 12. Environmental Data Initiative.
+  #     https://doi.org/10.6073/pasta/effe6d9f1a764ebaccb7ae9b28cba7a5
+  #
+  #   Saddle Stream site: 160, 1994-ongoing
+  #     Caine, T., J. Morse, S. Yevak, H. Brandes, and Niwot Ridge LTER.
+  #     2025. Stream water chemistry data for Saddle Stream site,
+  #     1994 - ongoing. ver 4. Environmental Data Initiative.
+  #     https://doi.org/10.6073/pasta/92e6f11e89b730321df0865bec4b7b0b
+  #
+  # Other packages available in this series if needed: 162 (watershed
+  # flume), 213 (Soddie).
   
   # Note: the overwrite argument does not work, so clear out any existing
-  # copies before running this
-  for (id in c(
-    "103", "108", "160",
-    "112", "163", "109"
-  )) {
-    # Ask EDI to tell me what the most current version is
+  # copies before running this loop.
+  for (id in c("103", "108", "160", "112", "163", "109")) {
     revision <- list_data_package_revisions(scope, id, filter = "newest")
-    
-    # Display current version - > this is referred to as the "packageID"
     packageID <- paste(scope, id, revision, sep = ".")
     
-    # Download the data
     read_data_package_archive(packageID, path = data_dir)
-    print(read_data_package_citation(packageID))
+    print(read_data_package_citation(packageID)) # confirm you're citing the version actually used
   }
-
-
-# update the below so you remember to cite it correctly
-## Albion: 103, 1982-ongoing
-  #Caine, T., J. Morse, S. Yevak, H. Brandes, and Niwot Ridge LTER. 2025. 
-  #Stream water chemistry data for Albion site, 1982 - ongoing. ver 15. 
-  #Environmental Data Initiative. https://doi.org/10.6073/pasta/39b37d16657733f1d661de4c727568a3 
   
-# Green Lake 4: 108, 1982-ongoing
-  #Caine, T., J. Morse, S. Yevak, H. Brandes, and Niwot Ridge LTER. 2025. 
-  #Stream water chemistry data for Green Lake 4, 1982 - ongoing. ver 14. 
-  #Environmental Data Initiative. https://doi.org/10.6073/pasta/cdbd25e2ce7a8b17f785bd4f07aba4fe
-  
-# Martinelli basin: 112, 1982-ongoing
-  #Caine, T., J. Morse, S. Yevak, H. Brandes, and Niwot Ridge LTER. 2025. 
-  #Stream water chemistry data for Martinelli basin, 1984 - ongoing. ver 5. 
-  #Environmental Data Initiative. https://doi.org/10.6073/pasta/4a761ed1f7b24d25c3eb870662213789 
-  
-# Green Lake 5 Rock Glacier: 163, 1998-ongoing
-  #Caine, T., J. Morse, S. Yevak, H. Brandes, and Niwot Ridge LTER. 2025. 
-  #Stream water chemistry data for Green Lake 5 Rock Glacier, 1998 - ongoing. ver 4. 
-  #Environmental Data Initiative. https://doi.org/10.6073/pasta/990a451879638d17eda4526215114390
-  
-# Green Lake 5 Outlet: 109, 1984- ongoing (?2021)
-  #Caine, T. 2021. 
-  #Stream water chemistry data for Green Lake 5 outlet, 1984 - ongoing. ver 12. 
-  #Environmental Data Initiative. https://doi.org/10.6073/pasta/effe6d9f1a764ebaccb7ae9b28cba7a5 
-
-# Saddle Stream site: 160, 1994-ongoing 
-  #Caine, T., J. Morse, S. Yevak, H. Brandes, and Niwot Ridge LTER. 2025. 
-  #Stream water chemistry data for Saddle Stream site, 1994 - ongoing. ver 4. 
-  #Environmental Data Initiative. https://doi.org/10.6073/pasta/92e6f11e89b730321df0865bec4b7b0b 
-  
-for (fname in list.files(data_dir,
-                         pattern = "knb-lter.*zip",
-                         full.names = TRUE
-)) {
-  unzip(zipfile = fname, exdir = data_dir)
-}
+  for (fname in list.files(data_dir, pattern = "knb-lter.*zip", full.names = TRUE)) {
+    unzip(zipfile = fname, exdir = data_dir)
+  }
 }
 
-# read in data --------------------------------------------------
 
-# all missing values that have to are MCAR
+# -- READ DATA ------------------------------------------------------------
+
+#  NA codes used across these files: not-collected, not-sampled, below
+# detection, etc. -- all treated as missing-completely-at-random (MCAR).
+
 na_vals <- c("", "NA", "NaN", "NP", "DNS", "NSS", "EQCL", "QNS", "NV", "dns")
 
-data_file <- list()
-data_file[["albisolu"]] <- read.csv(file.path(data_dir, "albisolu.nc.data.csv"),
-                                    na = na_vals)
-data_file[["gre4solu"]] <- read.csv(file.path(data_dir, "gre4solu.nc.data.csv"),
-                                    na = na_vals)
-data_file[["saddsolu"]] <- read.csv(file.path(data_dir, "saddsolu.nc.data.csv"),
-                                    na = na_vals)
-data_file[["martsolu"]] <- read.csv(file.path(data_dir, "martsolu.nc.data.csv"),
-                                    na = na_vals)
-data_file[["grrgsolu"]] <- read.csv(file.path(data_dir, "grrgsolu.nc.data.csv"),
-                                    na = na_vals)
-data_file[["gre5solu"]] <- read.csv(file.path(data_dir, "gre5solu.nc.data.csv"),
-                                    na = na_vals)
+site_files <- c(
+  albisolu = "albisolu.nc.data.csv",  # Albion
+  gre4solu = "gre4solu.nc.data.csv",  # Green Lake 4
+  saddsolu = "saddsolu.nc.data.csv",  # Saddle Stream
+  martsolu = "martsolu.nc.data.csv",  # Martinelli basin
+  grrgsolu = "grrgsolu.nc.data.csv",  # Green Lake 5 Rock Glacier
+  gre5solu = "gre5solu.nc.data.csv"   # Green Lake 5 Outlet
+)
 
+data_file <- lapply(
+  file.path(data_dir, site_files),
+  read.csv, na = na_vals
+)
 
 datafile <- plyr::rbind.fill(data_file) %>% data.frame(.)
 
+# -- CLEAN DATA -----------------------------------------------------------
+
 num_vars <- names(datafile)[6:44]
 
-# define replace function for non-numeric values
-repl.f <- function(x) ifelse(grepl("<|trace|u", x), 0, x)
+#Treat "trace", "<mdl", and similar non-numeric flags as 0. This may not
+# be the ideal treatment for every use case, but seems preferable to
+# dropping those records outright.
 
-# replace all trace, u, and <mdl values with 0 - this may
-# not be the best thing to do but seems better than leaving them out?
+repl.f <- function(x) ifelse(grepl("<|trace|u", x), 0, x)
 datafile <- datafile %>% dplyr::mutate_at(vars(num_vars), .funs = repl.f)
 
 
-# do by may, june (melt phase)
-# july, aug sept
+# Remove values >3 SD above that month's long-term average per site --
+# there are a handful of large outliers that look like data errors on
+# inspection.
 
-# nitrate, sulfateNO3./SO4..
-
-# remove high values >3sd greater than that months lt-average
-# as there seem to be some big outliers in there
 flag_vals <- datafile %>%
   mutate(
     local_site = ifelse(
@@ -166,7 +183,13 @@ flag_vals <- datafile %>%
   ) %>%
   select(local_site, month, n_cutoff, sulfate_cutoff)
 
-# make sulfate anoms by month for late season sulfur
+# ============================================================================
+# FIGURE 1: September sulfate anomaly, by site
+# ============================================================================
+# Late-season (September) sulfate is used here as an indicator of
+# late-summer sulfur flux; anomalies are percent difference from each
+# site's long-term September mean.
+
 chem_by_month <- datafile %>%
   mutate(
     local_site = ifelse(
@@ -189,7 +212,6 @@ chem_by_month <- datafile %>%
   )
 
 
-# make all into anomalies, by month
 anom_chem_month <- chem_by_month %>%
   filter(month == 9 & local_site %in% c(
     "ALBION", "GREEN LAKE 4", "GREEN LAKE 5",
@@ -206,27 +228,56 @@ anom_chem_month <- chem_by_month %>%
   ) %>%
   ungroup()
 
-# x-axis: force whole-number year breaks spaced ~5 apart (avoids
-# ggplot trying to cram a label for every single year), and angle the
-# text so labels don't collide with each other under the facets
+row_labels_S <- data.frame(
+  local_site = c("GREEN LAKE 4", "MARTINELLI"),
+  x = x_range_S[2] + diff(x_range_S) * 0.1,
+  y = 0.03 * diff(range(anom_chem_month$anom_S, na.rm = TRUE)),  # small upward nudge to visually center on 0
+  label = "more~sulfate  %<->%  less~sulfate"
+)
 
 g1 <- ggplot(anom_chem_month, aes(x = year, y = anom_S)) +
   geom_col(aes(fill = posneg)) +
   scale_fill_manual(values = c("#8C2F12", "#F5DDB0")) +
-  labs(y = "September sulfate anomaly (%)", x = "") +
-  scale_y_symmetric(sec.axis = sec_axis(
-    trans = I,
-    breaks = NULL, name = expression(more %<->% less)
-  )) +
+  labs(y = "September sulfate anomaly \n (% difference from long-term mean)", x = "Year") +
+  scale_x_continuous(
+    breaks = seq(
+      10 * floor(min(anom_chem_month$year) / 10),
+      10 * ceiling(max(anom_chem_month$year) / 10),
+      by = 10
+    ),
+    minor_breaks = seq(
+      5 * floor(min(anom_chem_month$year) / 5),
+      5 * ceiling(max(anom_chem_month$year) / 5),
+      by = 5
+    ),
+    guide = guide_axis(minor.ticks = TRUE)
+  ) +
+  scale_y_continuous(limits = symmetric_limits) +
+  geom_text(
+    data = row_labels_S,
+    aes(x = x, y = y, label = label),
+    angle = -90, hjust = 0.5, vjust = 0.5, size = 2.5,
+    inherit.aes = FALSE, parse = TRUE
+  ) +
   theme_hc() +
   facet_wrap(~local_site) +
-  theme(legend.position = "none")
+  theme(
+    legend.position = "none",
+    axis.minor.ticks.length = rel(0.5),
+    plot.margin = margin(t = 5.5, r = 25, b = 5.5, l = 5.5)  # extra right margin for the row label
+  ) +
+  coord_cartesian(xlim = x_range_S, clip = "off")
 
 ggsave(g1,
-  file = "figures/sulfate_anom_sept.png",
-  scale = 0.5, width = 15, height = 6
+       file = "figures/sulfate_anom_sept.png",
+       scale = 0.5, width = 15, height = 6
 )
 
+# ============================================================================
+# FIGURE 2: Summer (May-Sep) nitrate anomaly, by site
+# ============================================================================
+# Growing-season nitrate, averaged May through September; anomalies are
+# percent difference from each site's long-term summer mean.
 
 chem_by_summer <- datafile %>%
   mutate(
@@ -250,11 +301,8 @@ chem_by_summer <- datafile %>%
     .groups = "drop"
   )
 
-# make all into anomalies, by month or not
 anom_chem_summer_N <- chem_by_summer %>%
   filter(local_site %in% c("ALBION", "GREEN LAKE 4", "GREEN LAKE 5", "MARTINELLI")) %>%
-  # group_by(local_site, year) %>%
-  # summarise(mean_N = mean (nitrate, na.rm = TRUE, .groups = 'drop'))%>%
   group_by(local_site) %>%
   mutate(
     avg_N = mean(nitrate, na.rm = TRUE, .groups = "drop")
@@ -266,20 +314,48 @@ anom_chem_summer_N <- chem_by_summer %>%
   ) %>%
   ungroup()
 
+x_range_N <- range(anom_chem_summer_N$year, na.rm = TRUE)
+row_labels_N <- data.frame(
+  local_site = c("GREEN LAKE 4", "MARTINELLI"),  # rightmost facet of each row
+  x = x_range_N[2] + diff(x_range_N) * 0.1,
+  y = 0.03 * diff(range(anom_chem_summer_N$anom_N, na.rm = TRUE)),  # small upward nudge to visually center on 0
+  label = "more~nitrate  %<->%  less~nitrate"
+)
+
 g2 <- ggplot(anom_chem_summer_N, aes(x = year, y = anom_N)) +
   geom_col(aes(fill = posneg)) +
   scale_fill_manual(values = c("#1B7A6F", "#8ED1C4")) +
-  labs(y = "Summer Nitrate anomaly (%)", x = "") +
-  scale_y_symmetric(sec.axis = sec_axis(
-    trans = I,
-    breaks = NULL, name = expression(more %<->% less)
-  )) +
+  labs(y = "Summer nitrate anomaly \n (% difference from long-term mean)", x = "Year") +
+  scale_x_continuous(
+    breaks = seq(
+      10 * floor(min(anom_chem_summer_N$year) / 10),
+      10 * ceiling(max(anom_chem_summer_N$year) / 10),
+      by = 10
+    ),
+    minor_breaks = seq(
+      5 * floor(min(anom_chem_summer_N$year) / 5),
+      5 * ceiling(max(anom_chem_summer_N$year) / 5),
+      by = 5
+    ),
+    guide = guide_axis(minor.ticks = TRUE)
+  ) +
+  scale_y_continuous(limits = symmetric_limits) +
+  geom_text(
+    data = row_labels_N,
+    aes(x = x, y = y, label = label),
+    angle = -90, hjust = 0.5, vjust = 0.5, size = 2.5,
+    inherit.aes = FALSE, parse = TRUE
+  ) +
   theme_hc() +
   facet_wrap(~local_site) +
-  theme(legend.position = "none")
-
+  theme(
+    legend.position = "none",
+    axis.minor.ticks.length = rel(0.5),
+    plot.margin = margin(t = 5.5, r = 25, b = 5.5, l = 5.5)
+  ) +
+  coord_cartesian(xlim = x_range_N, clip = "off")
 
 ggsave(g2,
-  file = "figures/nitrate_anom_summer.png",
-  scale = 0.5, width = 15, height = 6
+       file = "figures/nitrate_anom_summer.png",
+       scale = 0.5, width = 15, height = 6
 )
